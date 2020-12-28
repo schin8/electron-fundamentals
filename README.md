8d0333ddbe86496b2ac82e0862728d26384c05c2
schin <steve.chin@zapatacomputing.com>
2020-12-28 13:26:23 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ipc-demo
+
+## Development notes.
+
+Use the `electron` version of `nodemon`
+
+Install globally
+> npm i -g electromon 
+
+Run this app
+> electromon src/main.js
+
+
---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Electron setup
+const electron = require('electron')
+const app = electron.app
+
+//
+
+app.on('ready!', _ => {
+    console.log('ready!')
+})
+console.log('ready!')
