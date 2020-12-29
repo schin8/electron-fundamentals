febf72e8abb84169aa74a664bb19585c57bd37a6
schin <steve.chin@zapatacomputing.com>
2020-12-28 20:35:34 -0500

    basic html support

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 // Electron setup
-const electron = require('electron')
-const app = electron.app
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
-//
+let mainWindow;
 
-app.on('ready', _ => {
-    console.log('ready!')
-})
-console.log('ready!')
+app.on("ready", (_) => {
+
+  mainWindow = new BrowserWindow({
+    height: 400,
+    width: 400,
+    webPreferences: {
+        nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/countdown.html`)
+
+  mainWindow.on("closed", (_) => {
+    mainWindow = null;
+    console.log("Cleanup");
+  });
+
+});
