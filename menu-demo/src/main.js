a1286462806a1521ab849c846fdbc46d0bc9d7c9
schin <steve.chin@zapatacomputing.com>
2021-01-16 10:47:40 -0500

    menu demo

--- menu-demo/src/main.js
+++ menu-demo/src/main.js
@@ -1,8 +1,43 @@
-const electron = require('electron')
+const { Accelerator } = require("electron")
+const electron = require("electron")
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
-app.on('ready', _ => {
-    //console.log("ready")
-    new BrowserWindow()
+// Feature - Menu
+const Menu = electron.Menu
+
+app.on("ready", (_) => {
+  new BrowserWindow({
+  })
+
+  const name = electron.app.getName()
+
+  const template = [
+    {
+      // retrieves from package.json productName (visible when packaged)
+      label: name,
+      submenu: [
+        {
+          label: `About ${name}`,
+          click: _ => {
+            console.log("clicked")
+          },
+          role: 'about'
+        },
+        {
+          type: "separator",
+        },
+        {
+          label: "Quit",
+          click: _ => {
+            app.quit()            
+          },
+          accelerator: 'Cmd+Q'
+        },
+      ],
+    },
+  ]
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
 })
